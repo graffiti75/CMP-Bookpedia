4f9df1939ff9e1fbe572b445e9e0883be5c8b6da
Rodrigo Cericatto <graffiti75@gmail.com>
2025-04-22 21:29:37 -0300

    Koin setup

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -5,9 +5,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.domain.Book
 import com.plcoding.bookpedia.book.presentation.book_list.BookListScreen
 import com.plcoding.bookpedia.book.presentation.book_list.BookListState
-import com.plcoding.bookpedia.book.presentation.book_list.books
 import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
 
 @Preview(showBackground = true)
@@ -22,6 +22,22 @@ fun BookSearchBarPreview() {
 				.fillMaxWidth()
 		)
 	}
+}
+
+private val books = (1..100).map {
+	Book(
+		id = it.toString(),
+		title = "Book $it",
+		imageUrl = "https://test.com",
+		authors = listOf("Author $it"),
+		description = "Description $it",
+		languages = emptyList(),
+		firstPublishYear = null,
+		averageRating = 4.67854,
+		ratingCount = 5,
+		numPages = 100,
+		numEditions = 3
+	)
 }
 
 @Preview(showBackground = true)
